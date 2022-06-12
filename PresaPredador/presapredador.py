ae26eb41f0093b3ec325267a9b25ba9358e70ac7
mateusvdcastro <mateusvc1124@gmail.com>
2022-06-12 18:27:21 -0300

    Update

--- PresaPredador/presapredador.py
+++ PresaPredador/presapredador.py
@@ -34,8 +34,8 @@ class Plots:
         """
         # Plota o gráfico de cada indivíduo em função do tempo
         plt.plot(T, plantas, label='Plantas')
-        plt.plot(T, rato, label='Coelho')
-        plt.plot(T, oncas, label='Oncas')
+        plt.plot(T, rato, label='Ratos')
+        plt.plot(T, oncas, label='Onças')
         plt.plot(T, vacas, label='Vacas')
         plt.plot(T, joaninhas, label='Joaninhas')
         plt.plot(T, coruja, label='Corujas')
@@ -48,7 +48,7 @@ class Plots:
     def PlotGeral(self, plantas=None, rato=None, oncas=None, vacas=None, joaninhas=None, coruja=None):
         fig, ax = plt.subplots(2, 2)
         ax[0, 0].plot(T, plantas, 'r', label='Grama')
-        ax[0, 0].plot(T, rato, 'b', label='Coelho')
+        ax[0, 0].plot(T, rato, 'b', label='Ratos')
         ax[0, 0].plot(T, oncas, 'g', label='Oncas')
         ax[0, 0].plot(T, vacas, 'y', label='Vacas')
         ax[0, 0].plot(T, joaninhas, 'c', label='Joaninhas')
@@ -59,7 +59,7 @@ class Plots:
         ax[0, 0].set_title('População')
 
         ax[0, 1].plot(T, plantas, 'r', label='Grama')
-        ax[0, 1].plot(T, rato, 'b', label='Coelho')
+        ax[0, 1].plot(T, rato, 'b', label='Ratos')
         ax[0, 1].plot(T, oncas, 'g', label='Oncas')
         ax[0, 1].plot(T, vacas, 'y', label='Vacas')
         ax[0, 1].plot(T, joaninhas, 'c', label='Joaninhas')
@@ -70,7 +70,7 @@ class Plots:
         ax[0, 1].set_title('População')
 
         ax[1, 0].plot(T, plantas, 'r', label='Grama')
-        ax[1, 0].plot(T, rato, 'b', label='Coelho')
+        ax[1, 0].plot(T, rato, 'b', label='Ratos')
         ax[1, 0].plot(T, oncas, 'g', label='Oncas')
         ax[1, 0].plot(T, vacas, 'y', label='Vacas')
         ax[1, 0].plot(T, joaninhas, 'c', label='Joaninhas')
@@ -81,7 +81,7 @@ class Plots:
         ax[1, 0].set_title('População')
 
         ax[1, 1].plot(T, plantas, 'r', label='Grama')
-        ax[1, 1].plot(T, rato, 'b', label='Coelho')
+        ax[1, 1].plot(T, rato, 'b', label='Ratos')
         ax[1, 1].plot(T, oncas, 'g', label='Oncas')
         ax[1, 1].plot(T, vacas, 'y', label='Vacas')
         ax[1, 1].plot(T, joaninhas, 'c', label='Joaninhas')
@@ -99,11 +99,11 @@ class Plots:
 
         """
         
-        plt.plot(joaninhas, plantas, label='--')
-        plt.plot(oncas, rato, label='--')
-        plt.plot(oncas, vacas, label='--')
-        plt.plot(plantas, joaninhas, label='--')
-        plt.plot(joaninhas, coruja, label='--')
+        plt.plot(joaninhas, plantas, label='Joaninhas X Plantas')
+        plt.plot(oncas, rato, label='Oncas X Ratos')
+        plt.plot(oncas, vacas, label='Oncas X Vacas')
+        plt.plot(plantas, joaninhas, label='Plantas X Joaninhas')
+        plt.plot(joaninhas, coruja, label='Joaninhas X Corujas')
         plt.xlabel('Espécies')
         plt.ylabel('Espécies')
         plt.title("Gráfico de Lotka-Volterra")
@@ -114,46 +114,46 @@ if __name__ == "__main__":
 
     # Cria o ambiente planta e seus coeficientes
 
-    plantas = Especie(N=15, p=[10, 0.1, 0.1, 0.2], k=100)
+    plantas = Especie(N=50, p=[10, 0.1, 0.1, 0.2], k=50)
 
     # Cria a espécie dos ratos e seus coeficientes
 
-    rato = Especie(N=23, p=[1.75, 0.175, 0.9], k=50)
+    rato = Especie(N=30, p=[1.75, 0.175, 0.9, 0.9], k=30)
 
     # Cria a espécie coelhos e seus coeficientes
 
-    oncas = Especie(N=15, p=[8.5, 0.1, 0.1], k=50)
+    oncas = Especie(N=15, p=[8.5, 0.1, 0.1], k=15)
 
     # Cria a espécie das vacas e seus coeficientes
 
-    vacas = Especie(N=15, p=[0.8, 0.9, 0.15], k=50)
+    vacas = Especie(N=40, p=[0.8, 0.5, 0.5], k=40)
 
     # Cria a espécie joaninha e seus coeficientes
 
-    joaninha = Especie(N=50, p=[2.25, 0.2, 0.09], k=50)
+    joaninha = Especie(N=35, p=[2.25, 0.95, 0.6], k=35)
 
     # Cria a espécie Passáro e seus coeficientes
 
-    coruja = Especie(N=100, p=[8.5, 0.01, 0.1], k=50)
+    coruja = Especie(N=30, p=[8.5, 0.5, 0.5], k=30)
 
     while(i < t-1):
         i += 1
 
-        plantas.N = plantas.vetor[i - 1] + plantas.vetor[i - 1] * (plantas.p[0] * (1 - plantas.vetor[i - 1] / plantas.k) - plantas.p[1]*rato.vetor[i - 1] - plantas.p[2]*joaninha.vetor[i - 1] - plantas.p[3]*vacas.vetor[i - 1]) * deltaT
+        plantas.N = plantas.vetor[i - 1] + plantas.vetor[i - 1] * (plantas.p[0] * (1 - plantas.vetor[i - 1] / plantas.k) - plantas.p[1]*vacas.vetor[i - 1] - plantas.p[2]*joaninha.vetor[i - 1] - plantas.p[3]*rato.vetor[i - 1]) * deltaT
 
         if (plantas.N < 0):
             plantas.N = 0
 
         plantas.vetor.append(plantas.N)
 
-        rato.N = rato.vetor[i-1] + rato.vetor[i-1] * (rato.p[1] * plantas.vetor[i - 1] - rato.p[2] * oncas.vetor[i - 1] - rato.p[0]) * deltaT
+        rato.N = rato.vetor[i-1] + rato.vetor[i-1] * (rato.p[1] * plantas.vetor[i - 1] - rato.p[2] * oncas.vetor[i - 1] - rato.p[3]*coruja.vetor[i - 1] - rato.p[0]) * deltaT
 
         if (rato.N < 0):
             rato.N = 0
 
         rato.vetor.append(rato.N)
 
-        oncas.N = oncas.vetor[i-1] + oncas.vetor[i-1] * (oncas.p[1] * rato.vetor[i - 1] + oncas.p[2]* vacas.vetor[i - 1] - oncas.p[0]) * deltaT
+        oncas.N = oncas.vetor[i-1] + oncas.vetor[i-1] * (oncas.p[1] * vacas.vetor[i - 1] + oncas.p[2]* rato.vetor[i - 1] - oncas.p[0]) * deltaT
 
         if (oncas.N < 0):
             oncas.N = 0
@@ -174,14 +174,13 @@ if __name__ == "__main__":
         
         joaninha.vetor.append(joaninha.N)
 
-        coruja.N = coruja.vetor[i-1] + coruja.vetor[i-1] * (coruja.p[1] * joaninha.vetor[i - 1] - coruja.p[0]) * deltaT
+        coruja.N = coruja.vetor[i-1] + coruja.vetor[i-1] * (coruja.p[1] * joaninha.vetor[i - 1] + coruja.p[2]*rato.vetor[i - 1] - coruja.p[0]) * deltaT
 
         if (coruja.N < 0):
             coruja.N = 0
         
         coruja.vetor.append(coruja.N)
 
-    # grama=None, coelho=None, oncas=None, vacas=None, joaninhas=None, coruja=None
     graficos = Plots()
 
     graficos.individuosPeloTempo(plantas=plantas.vetor, rato=rato.vetor, oncas=oncas.vetor, vacas=vacas.vetor, joaninhas=joaninha.vetor, coruja=coruja.vetor)
